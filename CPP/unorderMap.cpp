57f5763ac573894ef9552b26528a4e1623caa1f6
rohit sancheti <rohit.sancheti@gyanmatrix.com>
2020-07-18 14:21:52 +0530

    umap example added

--- CPP/unorderMap.cpp
+++ CPP/unorderMap.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #include <unordered_map>
 
 using namespace std;
@@ -18,5 +19,29 @@ int main()
     for (auto x : umap)
         cout << x.first << " " << x.second << endl;
 
+    string str = "geeks for geeks geeks quiz "
+                 "practice qa for";
+    printFrequencies(str);
+
     return 0;
+}
+
+// Prints frequencies of individual words in str
+void printFrequencies(const string &str)
+{
+    // declaring map of <string, int> type, each word
+    // is mapped to its frequency
+    unordered_map<string, int> wordFreq;
+
+    // breaking input into word using string stream
+    stringstream ss(str); // Used for breaking words
+    string word;          // To store individual words
+    while (ss >> word)
+        wordFreq[word]++;
+
+    // now iterating over word, freq pair and printing
+    // them in <, > format
+    unordered_map<string, int>::iterator p;
+    for (p = wordFreq.begin(); p != wordFreq.end(); p++)
+        cout << "(" << p->first << ", " << p->second << ")\n";
 }
